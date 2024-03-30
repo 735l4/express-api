bda46932b0aab3a146224558ff65e36ac7523f42
735l4 <pratibimbakhadka@gmail.com>
2024-03-30 23:37:14 +0545

    chore: update schema and ts config

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,12 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgSchema, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
-export const users = pgTable('users', {
+export const publicSchema = pgSchema("public");
+
+export const users = publicSchema.table('users', {
     id: serial('id').primaryKey(),
     name: text('name'),
     email: text('email').unique(),
     password: text('password'),
     createdAt: timestamp('created_at').notNull().defaultNow(),
-    updatedAt: timestamp('updated_at').notNull().defaultNow()
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
